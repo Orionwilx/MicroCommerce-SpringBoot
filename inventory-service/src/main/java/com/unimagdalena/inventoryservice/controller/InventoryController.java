186a4d5bc7c5b2910a5fb74329c1205141befa05
bragon23 <bjgm2001@gmail.com>
2025-05-21 00:32:18 -0500

    feat: Implementación de Circuit Breaker con Resilience4j en inventory-service, aun en pruebas

--- inventory-service/src/main/java/com/unimagdalena/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/unimagdalena/inventoryservice/controller/InventoryController.java
@@ -2,6 +2,7 @@ package com.unimagdalena.inventoryservice.controller;
 
 import com.unimagdalena.inventoryservice.entity.Inventory;
 import com.unimagdalena.inventoryservice.service.InventoryService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +21,15 @@ public class InventoryController {
     public Mono<ResponseEntity<Flux<Inventory>>> getAllInventoryItems() {
         return Mono.just(ResponseEntity.ok(inventoryService.getAllInventoryItems()));
     }
+
+
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "inventorygetById", fallbackMethod = "fallback")
     public Mono<ResponseEntity<Inventory>> getInventoryItemById(@PathVariable String id) {
+        if (id.equals("999")) {
+            throw new IllegalArgumentException("Simulated failure for ID: " + id);
+
+        }
         return inventoryService.getInventoryItemById(id)
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
@@ -44,5 +52,9 @@ public class InventoryController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteInventoryItem(@PathVariable String id) {
         return inventoryService.deleteInventoryItem(id);
+    }
+
+    String fallback(String id, Throwable t) {
+        return "Fallback: Could not retrieve item " + id + ". Please try again later.";
     }
 }
